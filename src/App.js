365687f7daecc86b46076e431d8ccec876265cad
Surafeljava <surafelk27@gmail.com>
2022-05-12 14:28:58 +0300

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './assets/download.png';
 import './app.css';
 import ItemCard from './componets/item_card';
+import { Button, TextField, Grid } from '@mui/material';
 
 const items = [
     {
@@ -17,13 +18,39 @@ const items = [
 
 function App(){
     return (
-        <div>
-            { items.map((item) => {
-                return (
-                    <ItemCard title={item.title} desc={item.desc} logo={item.logo}/>
-                );
-            }) }
-        </div>
+        <Grid container direction="row" width="100%">
+            <Grid container direction="column" xs={6}>
+                { items.map((item) => {
+                    return (
+                        <ItemCard title={item.title} desc={item.desc} logo={item.logo}/>
+                    );
+                }) }
+            </Grid>
+
+            <Grid item flexGrow={1}>
+            </Grid>
+
+            <Grid container direction="column" xs={3} alignItems="center">
+                <h1> Create Item </h1>
+                <TextField fullWidth variant='outlined' label='title'/>
+                <br />
+                <TextField fullWidth variant='outlined' label='description'/>
+                <br />
+                <Grid container direction="row" justifyContent="space-evenly">
+                    <Grid item xs={4}>
+                        <Button fullWidth variant='contained'> Add </Button>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <Button fullWidth variant='outlined'> Reset </Button>
+                    </Grid>
+                </Grid>
+
+            </Grid>
+
+            <Grid item flexGrow={1}>
+            </Grid>
+        </Grid>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
+import { CssBaseline } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    <App />
+    <div>
+      <CssBaseline/>
+      <App />
+    </div>
   </React.StrictMode>
 );
